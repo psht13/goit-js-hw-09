9419c695c91337779ec9147e1372ebd24dfc24e8
paul_yurchenko <pashtet.gm@gmail.com>
2024-06-01 16:13:13 +0300

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,22 +3,26 @@ const formData = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
   email: '',
   message: '',
 };
-form.elements.email.value = formData.email; // присвоюю початкові значення при завантаженні
-form.elements.message.value = formData.message;
+form.elements.email.value = formData.email.trim(); // присвоюю початкові значення при завантаженні
+form.elements.message.value = formData.message.trim();
 
 form.addEventListener('input', () => {
-  formData.email = form.elements.email.value;
-  formData.message = form.elements.message.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData)); // завантажую у
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData)); // завантажую у сховище
 });
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  if (!form.elements.email.value || !form.elements.message.value) {
+  if (
+    !form.elements.email.value.trim() ||
+    !form.elements.message.value.trim()
+  ) {
     // перевіряю на порожній ввід
     alert('Fill please all fields');
     return;
   }
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.removeItem('feedback-form-state');
   form.reset();
 });
